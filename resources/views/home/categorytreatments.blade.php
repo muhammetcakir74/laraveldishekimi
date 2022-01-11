03c7cfba8191c083f0bbc3fa6eb62fe6df4cdef1
muhammetcakir74 <7467muhammetcakir@gmail.com>
2022-01-11 23:13:46 +0300

    video25

---
+++ resources/views/home/categorytreatments.blade.php
@@ -0,0 +1,14 @@
+@extends("layouts.home")
+@section('title',$setting->title . "Products List")
+
+
+@section('description'){{$setting->description}}@endsection
+
+@section('keywords',$setting->keywords)
+
+
+@section('content')
+
+
+
+@endsection
