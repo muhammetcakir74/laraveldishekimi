4b20ec598e7dda5355052bf2f7f05d50704aa5fb
muhammetcakir74 <7467muhammetcakir@gmail.com>
2022-01-14 00:01:58 +0300

    video28-2

---
+++ resources/views/home/randevu.blade.php
@@ -0,0 +1,14 @@
+@extends("layouts.home")
+@section('title',$setting->title . "")
+
+
+@section('description'){{$setting->description}}@endsection
+
+@section('keywords',$setting->keywords)
+
+
+@section('content')
+
+
+
+@endsection
