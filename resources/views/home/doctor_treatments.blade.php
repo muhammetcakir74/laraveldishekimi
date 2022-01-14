5d138d82ad64ec9713fabb7c43ba4873168ea8e5
muhammetcakir74 <7467muhammetcakir@gmail.com>
2022-01-14 04:38:08 +0300

    video29

---
+++ resources/views/home/doctor_treatments.blade.php
@@ -0,0 +1,92 @@
+@extends("layouts.home")
+@section('title','Your Treatments')
+
+
+
+
+
+@section('content')
+
+    <!-- Breadcrumb Section Begin -->
+    <section class="breadcrumb-section set-bg" data-setbg="{{asset('assets')}}/img/breadcrumb.jpg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="breadcrumb__text">
+                        <h2>My Reviews</h2>
+                        <div class="breadcrumb__option">
+                            <a href="{{route('home')}}">Home</a>
+                            <span>User</span>
+                            <span> / My Reviews</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Breadcrumb Section End -->
+
+<section class="product spad">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-5">
+                <div class="sidebar">
+                    <div class="sidebar__item p-3 mb-2 bg-dark text-white" style=" border-radius: 10px 10px 10px 10px;">
+                        <h4 class="text-danger" style="border-bottom: 4px solid white;margin-bottom: 10px!important;padding-bottom: 10px;">User Panel</h4>
+                        <ul>
+                            <li><a style="color: white;" href="{{route('profile.show')}}">My Profile</a></li>
+                            <li><a style="color: white;" href="{{route('doctor_randevu_show')}}">My Treatments</a></li>
+                            <li><a style="color: white;" href="{{route('logout')}}">Logout</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+
+            <script>
+                $(document).ready(function () {
+                    setTimeout(function () {
+                        $("div.alert-block").fadeOut("slow", function () {
+                            $("div.alert-block").remove();
+                        });
+                    }, 5000);
+                });
+            </script>
+
+
+            <div class="col-lg-9 col-md-7">
+                <h3 style="margin-bottom: 2rem;margin-top: -4rem;">Your Reviews</h3>
+                @include('home.message')
+
+                <table id="customers">
+                    <tr>
+                        <th>Hasta Adı</th>
+                        <th>Tedavi Adı</th>
+                        <th>Tarih</th>
+                        <th>Saat</th>
+                        <th>Hasta Notu</th>
+                        <th>Date</th>
+                        <th>Actions</th>
+                    </tr>
+                    @foreach($datalist as $rs)
+                        <tr>
+                            <td>{{$rs->id}}</td>
+                            <td>
+                                <a href="{{route('treatment',['id'=>$rs->treatment->id,'slug'=>$rs->treatment->slug])}}" target="_blank">
+                                    {{$rs->treatment->title}}
+                                </a>
+                            </td>
+                            <td>{{$rs->subject}}</td>
+                            <td>{{$rs->review}}</td>
+                            <td>{{$rs->rate}}</td>
+                            <td>{{$rs->status}}</td>
+                            <td>{{$rs->created_at}}</td>
+                            <td><a href="{{route('user_review_delete',['id'=>$rs->id])}}" onclick="return confirm('Delete Are you sure ?')"><img src="{{asset('assets')}}/img/delete-button.png" width="25px"></a></td>
+                        </tr>
+                    @endforeach
+                </table>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
